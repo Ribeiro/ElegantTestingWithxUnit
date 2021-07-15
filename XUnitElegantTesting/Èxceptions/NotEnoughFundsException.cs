84d7cfa41fada5313015165d9cafeca0b9edde1e
Geovanny Ribeiro <geovanny.ribeiro@gmail.com>
2021-07-14 23:10:05 -0300

    Refactored AppException to include common Exception constructors

--- XUnitElegantTesting/Èxceptions/NotEnoughFundsException.cs
+++ XUnitElegantTesting/Èxceptions/NotEnoughFundsException.cs
@@ -5,7 +5,10 @@ namespace XUnitElegantTesting.Èxceptions
 {
     public class NotEnoughFundsException : Exception
     {
+        public NotEnoughFundsException() { }
+
         public NotEnoughFundsException(string message) : base(message) { }
 
+        public NotEnoughFundsException(string message, Exception innerException) : base(message, innerException) { }
     }
 }
